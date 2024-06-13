ee98176b90f722f00d1135138eeb7833cd4ad350
Darshan_Moradiya <darshancodes99@gmail.com>
2024-06-13 14:13:28 +0530

    setup

---
+++ src/main/java/com/codes/darshan/demoProject/DarshanCodesApplication.java
@@ -0,0 +1,13 @@
+package com.codes.darshan.demoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarshanCodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarshanCodesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codes/darshan/demoProject/DarshanCodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codes.darshan.demoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarshanCodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
